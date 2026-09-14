729afa8f40549823c0d25e08b23dfbb3e0163f2a
tymoemeil-ux <281043502+tymoemeil-ux@users.noreply.github.com>
2026-09-14 19:11:13 +0000

    Fix: brakujace średniki w _config.glsl (błąd kompilacji shadera)
    Każdy blok TEXT_EFFECT musiał kończyć się średnikiem - bez niego
    kompilator GLSL zgłaszał 'unexpected }' i pack nie startował.
    Zweryfikowane kompilacją glslang dla wszystkich wersji (base,
    1.21.2, 1.21.6, 26.1, 26.2) z porównaniem do oryginału.
    
    Co-authored-by: arena-agent <297053741+arena-agent@users.noreply.github.com>

--- Animacje100/assets/minecraft/shaders/include/_config.glsl
+++ Animacje100/assets/minecraft/shaders/include/_config.glsl
@@ -1,214 +1,214 @@
 TEXT_EFFECT_WITH_SHADOW(rgb(248, 248, 0)) {
     apply_wavy();
-    apply_color(rgb(80, 255, 80))
+    apply_color(rgb(80, 255, 80));
 }
 
 TEXT_EFFECT_WITH_SHADOW(rgb(248, 248, 4)) {
     apply_wavy(12000.0, 1.5);
-    apply_color(rgb(255, 255, 255))
+    apply_color(rgb(255, 255, 255));
 }
 
 TEXT_EFFECT_WITH_SHADOW(rgb(248, 248, 8)) {
     apply_wavy(4000.0, 0.8);
-    apply_color(rgb(120, 180, 255))
+    apply_color(rgb(120, 180, 255));
 }
 
 TEXT_EFFECT_WITH_SHADOW(rgb(248, 248, 12)) {
     apply_wavy(24000.0, 1.0);
-    apply_color(rgb(0, 255, 255))
+    apply_color(rgb(0, 255, 255));
 }
 
 TEXT_EFFECT_WITH_SHADOW(rgb(248, 248, 16)) {
     apply_wavy(8000.0, 0.35);
-    apply_color(rgb(255, 255, 200))
+    apply_color(rgb(255, 255, 200));
 }
 
 TEXT_EFFECT_WITH_SHADOW(rgb(248, 248, 20)) {
     apply_shake();
-    apply_color(rgb(255, 80, 80))
+    apply_color(rgb(255, 80, 80));
 }
 
 TEXT_EFFECT_WITH_SHADOW(rgb(248, 248, 24)) {
     apply_shake(2.0, 1.5);
-    apply_color(rgb(255, 220, 80))
+    apply_color(rgb(255, 220, 80));
 }
 
 TEXT_EFFECT_WITH_SHADOW(rgb(248, 248, 28)) {
     apply_shake(1.0, 5.0);
-    apply_color(rgb(255, 255, 255))
+    apply_color(rgb(255, 255, 255));
 }
 
 TEXT_EFFECT_WITH_SHADOW(rgb(248, 248, 32)) {
     apply_shake(3.0, 0.5);
-    apply_color(rgb(200, 200, 255))
+    apply_color(rgb(200, 200, 255));
 }
 
 TEXT_EFFECT(rgb(248, 248, 36)) {
-    apply_rainbow()
+    apply_rainbow();
 }
 
 TEXT_EFFECT(rgb(248, 248, 40)) {
-    apply_rainbow(1200.0)
+    apply_rainbow(1200.0);
 }
 
 TEXT_EFFECT_WITH_SHADOW(rgb(248, 248, 44)) {
     apply_bouncy();
-    apply_color(rgb(255, 170, 0))
+    apply_color(rgb(255, 170, 0));
 }
 
 TEXT_EFFECT_WITH_SHADOW(rgb(248, 248, 48)) {
     apply_bouncy(3000.0, 3.0);
-    apply_color(rgb(255, 255, 255))
+    apply_color(rgb(255, 255, 255));
 }
 
 TEXT_EFFECT_WITH_SHADOW(rgb(248, 248, 52)) {
     apply_blinking();
-    apply_color(rgb(80, 80, 255))
+    apply_color(rgb(80, 80, 255));
 }
 
 TEXT_EFFECT_WITH_SHADOW(rgb(248, 248, 56)) {
     apply_blinking(0.25);
-    apply_color(rgb(80, 255, 80))
+    apply_color(rgb(80, 255, 80));
 }
 
 TEXT_EFFECT_WITH_SHADOW(rgb(248, 248, 60)) {
     apply_pulse();
-    apply_color(rgb(80, 255, 255))
+    apply_color(rgb(80, 255, 255));
 }
 
 TEXT_EFFECT_WITH_SHADOW(rgb(248, 248, 64)) {
     apply_pulse(20.0, 0.8);
-    apply_color(rgb(255, 255, 255))
+    apply_color(rgb(255, 255, 255));
 }
 
 TEXT_EFFECT_WITH_SHADOW(rgb(248, 248, 68)) {
     apply_heartbeat();
-    apply_color(rgb(255, 60, 60))
+    apply_color(rgb(255, 60, 60));
 }
 
 TEXT_EFFECT_WITH_SHADOW(rgb(248, 248, 72)) {
     apply_spin();
-    apply_color(rgb(255, 80, 255))
+    apply_color(rgb(255, 80, 255));
 }
 
 TEXT_EFFECT_WITH_SHADOW(rgb(248, 248, 76)) {
     apply_spin(1000.0);
-    apply_color(rgb(255, 255, 255))
+    apply_color(rgb(255, 255, 255));
 }
 
 TEXT_EFFECT_WITH_SHADOW(rgb(248, 248, 80)) {
     apply_sequential_spin();
-    apply_color(rgb(255, 255, 255))
+    apply_color(rgb(255, 255, 255));
 }
 
 TEXT_EFFECT_WITH_SHADOW(rgb(248, 248, 84)) {
     apply_sequential_spin(5000.0);
-    apply_color(rgb(255, 255, 80))
+    apply_color(rgb(255, 255, 80));
 }
 
 TEXT_EFFECT_WITH_SHADOW(rgb(248, 248, 88)) {
     apply_fade();
-    apply_color(rgb(255, 255, 80))
+    apply_color(rgb(255, 255, 80));
 }
 
 TEXT_EFFECT_WITH_SHADOW(rgb(248, 248, 92)) {
     apply_breathe();
-    apply_color(rgb(120, 255, 120))
+    apply_color(rgb(120, 255, 120));
 }
 
 TEXT_EFFECT_WITH_SHADOW(rgb(248, 248, 96)) {
     apply_flicker();
-    apply_color(rgb(255, 255, 255))
+    apply_color(rgb(255, 255, 255));
 }
 
 TEXT_EFFECT_WITH_SHADOW(rgb(248, 248, 100)) {
     apply_iterating();
-    apply_color(rgb(170, 0, 255))
+    apply_color(rgb(170, 0, 255));
 }
 
 TEXT_EFFECT_WITH_SHADOW(rgb(248, 248, 104)) {
     apply_glitch();
-    apply_color(rgb(255, 80, 80))
+    apply_color(rgb(255, 80, 80));
 }
 
 TEXT_EFFECT_WITH_SHADOW(rgb(248, 248, 108)) {
     apply_glitch(0.5, 1.0);
-    apply_color(rgb(200, 200, 255))
+    apply_color(rgb(200, 200, 255));
 }
 
 TEXT_EFFECT_WITH_SHADOW(rgb(248, 248, 112)) {
     apply_scale(1.5);
-    apply_color(rgb(80, 255, 255))
+    apply_color(rgb(80, 255, 255));
 }
 
 TEXT_EFFECT_WITH_SHADOW(rgb(248, 248, 116)) {
     apply_scale(2.5);
-    apply_color(rgb(255, 120, 255))
+    apply_color(rgb(255, 120, 255));
 }
 
 TEXT_EFFECT_WITH_SHADOW(rgb(248, 248, 120)) {
     apply_scale(0.8);
-    apply_color(rgb(255, 255, 255))
+    apply_color(rgb(255, 255, 255));
 }
 
 TEXT_EFFECT_WITH_SHADOW(rgb(248, 248, 124)) {
     apply_offset(0.0, 40.0);
-    apply_color(rgb(255, 170, 0))
+    apply_color(rgb(255, 170, 0));
 }
 
 TEXT_EFFECT_WITH_SHADOW(rgb(248, 248, 128)) {
     apply_offset(0.0, -30.0);
-    apply_color(rgb(80, 255, 255))
+    apply_color(rgb(80, 255, 255));
 }
 
 TEXT_EFFECT_WITH_SHADOW(rgb(248, 248, 132)) {
     apply_bob();
-    apply_color(rgb(120, 200, 255))
+    apply_color(rgb(120, 200, 255));
 }
 
 TEXT_EFFECT_WITH_SHADOW(rgb(248, 248, 136)) {
     apply_sway();
-    apply_color(rgb(100, 255, 200))
+    apply_color(rgb(100, 255, 200));
 }
 
 TEXT_EFFECT_WITH_SHADOW(rgb(248, 248, 140)) {
     apply_zigzag();
-    apply_color(rgb(255, 255, 0))
+    apply_color(rgb(255, 255, 0));
 }
 
 TEXT_EFFECT_WITH_SHADOW(rgb(248, 248, 144)) {
     apply_wobble();
-    apply_color(rgb(255, 150, 255))
+    apply_color(rgb(255, 150, 255));
 }
 
 TEXT_EFFECT_WITH_SHADOW(rgb(248, 248, 148)) {
     apply_fall();
-    apply_color(rgb(140, 140, 140))
+    apply_color(rgb(140, 140, 140));
 }
 
 TEXT_EFFECT_WITH_SHADOW(rgb(248, 248, 152)) {
     apply_rise();
-    apply_color(rgb(120, 255, 120))
+    apply_color(rgb(120, 255, 120));
 }
 
 TEXT_EFFECT_WITH_SHADOW(rgb(248, 248, 156)) {
     apply_orbit();
-    apply_color(rgb(255, 200, 80))
+    apply_color(rgb(255, 200, 80));
 }
 
 TEXT_EFFECT_WITH_SHADOW(rgb(248, 248, 160)) {
     apply_drift();
-    apply_color(rgb(150, 220, 255))
+    apply_color(rgb(150, 220, 255));
 }
 
 TEXT_EFFECT_WITH_SHADOW(rgb(248, 248, 164)) {
     apply_typewriter();
-    apply_color(rgb(180, 255, 180))
+    apply_color(rgb(180, 255, 180));
 }
 
 TEXT_EFFECT_WITH_SHADOW(rgb(248, 248, 168)) {
     apply_erase();
-    apply_color(rgb(255, 100, 100))
+    apply_color(rgb(255, 100, 100));
 }
 
 TEXT_EFFECT(rgb(248, 248, 172)) {
@@ -224,11 +224,11 @@ TEXT_EFFECT(rgb(248, 248, 180)) {
 }
 
 TEXT_EFFECT(rgb(248, 248, 184)) {
-    apply_lava()
+    apply_lava();
 }
 
 TEXT_EFFECT(rgb(248, 248, 188)) {
-    apply_aurora()
+    apply_aurora();
 }
 
 TEXT_EFFECT(rgb(248, 248, 192)) {
@@ -245,12 +245,12 @@ TEXT_EFFECT(rgb(248, 248, 200)) {
 
 TEXT_EFFECT(rgb(248, 248, 204)) {
     apply_split();
-    apply_color(rgb(255, 100, 200))
+    apply_color(rgb(255, 100, 200));
 }
 
 TEXT_EFFECT(rgb(248, 248, 208)) {
     apply_outline(rgb(0, 0, 0), 1.0);
-    apply_color(rgb(255, 255, 255))
+    apply_color(rgb(255, 255, 255));
 }
 
 TEXT_EFFECT(rgb(248, 248, 212)) {
@@ -268,27 +268,27 @@ TEXT_EFFECT(rgb(248, 248, 220)) {
 
 TEXT_EFFECT(rgb(248, 248, 224)) {
     apply_chromatic(1.0, 1.0);
-    apply_color(rgb(255, 255, 255))
+    apply_color(rgb(255, 255, 255));
 }
 
 TEXT_EFFECT(rgb(248, 248, 228)) {
     apply_extrude(1.0, 3.0);
-    apply_color(rgb(255, 200, 0))
+    apply_color(rgb(255, 200, 0));
 }
 
 TEXT_EFFECT(rgb(248, 248, 232)) {
     apply_extrude(2.0, 6.0, rgb(80, 40, 0));
-    apply_color(rgb(255, 220, 100))
+    apply_color(rgb(255, 220, 100));
 }
 
 TEXT_EFFECT(rgb(248, 248, 236)) {
     apply_noise(1.0, 1.0);
-    apply_color(rgb(255, 255, 255))
+    apply_color(rgb(255, 255, 255));
 }
 
 TEXT_EFFECT(rgb(248, 248, 240)) {
     apply_liquid(1.0, 1.0);
-    apply_color(rgb(80, 200, 220))
+    apply_color(rgb(80, 200, 220));
 }
 
 TEXT_EFFECT(rgb(248, 248, 244)) {
@@ -303,7 +303,7 @@ TEXT_EFFECT(rgb(248, 248, 248)) {
 
 TEXT_EFFECT(rgb(240, 240, 0)) {
     apply_wavy();
-    apply_rainbow()
+    apply_rainbow();
 }
 
 TEXT_EFFECT(rgb(240, 240, 4)) {
@@ -313,7 +313,7 @@ TEXT_EFFECT(rgb(240, 240, 4)) {
 
 TEXT_EFFECT(rgb(240, 240, 8)) {
     apply_wavy();
-    apply_aurora()
+    apply_aurora();
 }
 
 TEXT_EFFECT(rgb(240, 240, 12)) {
@@ -333,7 +333,7 @@ TEXT_EFFECT(rgb(240, 240, 20)) {
 
 TEXT_EFFECT(rgb(240, 240, 24)) {
     apply_shake();
-    apply_rainbow()
+    apply_rainbow();
 }
 
 TEXT_EFFECT(rgb(240, 240, 28)) {
@@ -343,12 +343,12 @@ TEXT_EFFECT(rgb(240, 240, 28)) {
 
 TEXT_EFFECT_WITH_SHADOW(rgb(240, 240, 32)) {
     apply_shake();
-    apply_glitch()
+    apply_glitch();
 }
 
 TEXT_EFFECT(rgb(240, 240, 36)) {
     apply_bouncy();
-    apply_rainbow()
+    apply_rainbow();
 }
 
 TEXT_EFFECT(rgb(240, 240, 40)) {
@@ -363,7 +363,7 @@ TEXT_EFFECT(rgb(240, 240, 44)) {
 
 TEXT_EFFECT(rgb(240, 240, 48)) {
     apply_pulse();
-    apply_rainbow()
+    apply_rainbow();
 }
 
 TEXT_EFFECT(rgb(240, 240, 52)) {
@@ -378,7 +378,7 @@ TEXT_EFFECT(rgb(240, 240, 56)) {
 
 TEXT_EFFECT(rgb(240, 240, 60)) {
     apply_spin();
-    apply_rainbow()
+    apply_rainbow();
 }
 
 TEXT_EFFECT(rgb(240, 240, 64)) {
@@ -388,17 +388,17 @@ TEXT_EFFECT(rgb(240, 240, 64)) {
 
 TEXT_EFFECT(rgb(240, 240, 68)) {
     apply_sequential_spin();
-    apply_aurora()
+    apply_aurora();
 }
 
 TEXT_EFFECT(rgb(240, 240, 72)) {
     apply_fade();
-    apply_rainbow()
+    apply_rainbow();
 }
 
 TEXT_EFFECT(rgb(240, 240, 76)) {
     apply_bob();
-    apply_rainbow()
+    apply_rainbow();
 }
 
 TEXT_EFFECT(rgb(240, 240, 80)) {
@@ -413,17 +413,17 @@ TEXT_EFFECT(rgb(240, 240, 84)) {
 
 TEXT_EFFECT_WITH_SHADOW(rgb(240, 240, 88)) {
     apply_zigzag();
-    apply_bouncy()
+    apply_bouncy();
 }
 
 TEXT_EFFECT(rgb(240, 240, 92)) {
     apply_wobble();
-    apply_rainbow()
+    apply_rainbow();
 }
 
 TEXT_EFFECT(rgb(240, 240, 96)) {
     apply_orbit();
-    apply_rainbow()
+    apply_rainbow();
 }
 
 TEXT_EFFECT(rgb(240, 240, 100)) {
@@ -438,23 +438,23 @@ TEXT_EFFECT(rgb(240, 240, 104)) {
 
 TEXT_EFFECT_WITH_SHADOW(rgb(240, 240, 108)) {
     apply_erase();
-    apply_glitch()
+    apply_glitch();
 }
 
 TEXT_EFFECT(rgb(240, 240, 112)) {
     apply_glitch();
     apply_chromatic(1.5, 1.0);
-    apply_color(rgb(255, 255, 255))
+    apply_color(rgb(255, 255, 255));
 }
 
 TEXT_EFFECT(rgb(240, 240, 116)) {
     apply_shake();
-    apply_lava()
+    apply_lava();
 }
 
 TEXT_EFFECT(rgb(240, 240, 120)) {
     apply_extrude(1.0, 3.0);
-    apply_lava()
+    apply_lava();
 }
 
 TEXT_EFFECT(rgb(240, 240, 124)) {
@@ -471,23 +471,23 @@ TEXT_EFFECT(rgb(240, 240, 128)) {
 TEXT_EFFECT(rgb(240, 240, 132)) {
     apply_glitch();
     apply_noise(1.0, 1.0);
-    apply_color(rgb(255, 255, 255))
+    apply_color(rgb(255, 255, 255));
 }
 
 TEXT_EFFECT(rgb(240, 240, 136)) {
     apply_shake();
     apply_split();
-    apply_color(rgb(255, 100, 200))
+    apply_color(rgb(255, 100, 200));
 }
 
 TEXT_EFFECT(rgb(240, 240, 140)) {
     apply_bouncy();
     apply_outline(rgb(0, 0, 0), 1.0);
-    apply_color(rgb(255, 255, 255))
+    apply_color(rgb(255, 255, 255));
 }
 
 TEXT_EFFECT(rgb(240, 240, 144)) {
     apply_wavy();
     apply_shake();
-    apply_rainbow()
+    apply_rainbow();
 }
