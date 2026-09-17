16fd3314693232a4cc0533bd3c731c02670e88f8
tymoemeil-ux <281043502+tymoemeil-ux@users.noreply.github.com>
2026-09-17 19:51:44 +0000

    v1.2.7: marker paperweight-mappings-namespace=mojang w manifeście (plugin bez internals = pomijaj PluginRemapper) - fix CNFE Material na Paper Moonrise
    Co-authored-by: arena-agent <297053741+arena-agent@users.noreply.github.com>

--- plugin/build.sh
+++ plugin/build.sh
@@ -11,7 +11,7 @@ javac -encoding UTF-8 --release 17 \
 # tylko pl/ do jarze (stubs org/bukkit sa tylko po to, zeby sie skompilowalo)
 mkdir -p build/stage
 cp -r build/classes/pl build/stage/
-jar cf AnimacjeHub.jar \
+jar cfm AnimacjeHub.jar build/manifest.txt \
   -C build/stage . \
   -C res plugin.yml \
   -C res config.yml \
